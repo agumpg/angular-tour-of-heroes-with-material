60fa4f2bf7b244639aaa58009965d46cbe4dc0d4
A. Pereña <agumpg@gmail.com>
2020-07-06 09:42:08 +0200

    get y update con InMemoryDataBase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { HeroDetailComponent } from './components/hero-detail/hero-detail.compon
 import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SwitchLogsComponent } from './components/switch-logs/switch-logs.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 @NgModule( {
   declarations: [
@@ -29,7 +33,18 @@ import { SwitchLogsComponent } from './components/switch-logs/switch-logs.compon
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -4,12 +4,13 @@ import { ActivatedRoute } from '@angular/router';
 import { HeroService } from '../../services/hero.service';
 import { Location } from '@angular/common';
 import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
-@Component({
+@Component( {
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
-})
+} )
 export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
@@ -24,20 +25,27 @@ export class HeroDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.getHero();
-    this.buildForm();
+    this.getHero()
+      .subscribe(
+        hero => {
+          this.hero = hero;
+          this.buildForm();
+        } );
   }
 
-  getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(
-        hero => { this.hero = hero; });
+  getHero(): Observable<Hero> {
+    const id = +this.route.snapshot.paramMap.get( 'id' );
+    return this.heroService.getHero( id );
+
   }
 
   saveHero(): void {
-    this.heroService.saveHero(this.hero.id, this.nameField.value);
-    this.location.back();
+    /*  this.heroService.saveHero(this.hero.id, this.nameField.value);
+     this.location.back(); */
+
+    this.hero.name = this.nameField.value;
+    this.heroService.updateHero( this.hero )
+      .subscribe( () => this.goBack() );
   }
 
   goBack(): void {
@@ -46,16 +54,16 @@ export class HeroDetailComponent implements OnInit {
 
   // GETTERs de campos del formulario
   get idField(): AbstractControl {
-    return this.formHero.get('id');
+    return this.formHero.get( 'id' );
   }
 
   get nameField(): AbstractControl {
-    return this.formHero.get('name');
+    return this.formHero.get( 'name' );
   }
 
   // Lectura de errores de campos
   getNameFieldErrors(): string {
-    if (this.nameField.hasError('maxlength')) {
+    if ( this.nameField.hasError( 'maxlength' ) ) {
       return 'Nombre demasiado largo. Introduce un nombre de hasta 20 caracteres';
     }
     return 'El nombre no puede ser vacío. Debe introducir un nombre de héroe';
@@ -63,10 +71,10 @@ export class HeroDetailComponent implements OnInit {
 
   // Construcción del Formulario
   private buildForm(): void {
-    this.formHero = this.formBuilder.group({
+    this.formHero = this.formBuilder.group( {
       id: [{ value: this.hero.id, disabled: true }],
-      name: [this.hero.name, [Validators.required, Validators.maxLength(20)]]
-    });
+      name: [this.hero.name, [Validators.required, Validators.maxLength( 20 )]]
+    } );
   }
 
 }
